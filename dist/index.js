30c072f9e471942cbcf178f904039288e5e2abde
Smos Bois <simondeb1505@gmail.com>
2022-05-25 23:20:19 +0200

    Trying to make it pretty

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,131 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 
-const canvas = document.querySelector('.webgl')
+//loading
+const textureLoader = new THREE.TextureLoader()
+
+const normalTexture = textureLoader.load('/textures/SmallBricks.jpg')
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({
-	color:0x00ff00
-})
-const mesh = new THREE.Mesh(geometry,material)
-scene.add(mesh)
+// Objects
+const geometry = new THREE.SphereGeometry( 1, 32, 16 );
+
+// Materials
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.2
+material.roughness = 0.6
+material.wireframe = true
+//material.normalMap = normalTexture
+material.color = new THREE.Color(0xffffff)
+
+// Mesh
+const sphere = new THREE.Mesh(geometry,material)
+scene.add(sphere)
+
+// Lights
+
+//light 1
+/*
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+*/
+
+//light 2
+const pointLight2 = new THREE.PointLight(0x4176d9, 0.1)
+pointLight2.position.set(-2.25, 1.2,4.64)
+pointLight2.intensity = 2
+scene.add(pointLight2)
+
+//const pointLightHelper = new THREE.PointLightHelper(pointLight2, 0.1)
+//scene.add(pointLightHelper)
+
+//light 3
+const pointLight3 = new THREE.PointLight(0x52166e, 0.1)
+pointLight3.position.set(1.13,-2.37,-6)
+pointLight3.intensity = 1.6
+scene.add(pointLight3)
+
+//const pointLightHelper3 = new THREE.PointLightHelper(pointLight3, 0.1)
+//scene.add(pointLightHelper3)
 
 
+/**
+ * Sizes
+ */
 const sizes = {
-	width: window.innerWidth,
-	height: window.innerHeight
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
-camera.position.set(0,1,2)
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
 scene.add(camera)
 
-const renderer = new THREE.WebGL1Renderer({
-	canvas: canvas
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: false
 })
-
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
-renderer.shadowMap.enabled = true
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-function animate(){
-	requestAnimationFrame(animate)
-	renderer.render(scene, camera)
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = .1 * elapsedTime
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
 
-animate()
+tick()
