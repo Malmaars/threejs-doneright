b0cae4f48f67d0bd580c4b8142feef5ee7b142a4
Smos Bois <simondeb1505@gmail.com>
2022-06-01 21:17:48 +0200

    Updates

--- js/Boat.js
+++ js/Boat.js
@@ -8,7 +8,7 @@ function Boat(scene, loadingManager) {
     // load a resource
     texLoader.load(
         // resource URL
-        'resources/Textures/BoatTextures/lambert1_Base_color.png',
+        'resources/textures/BoatTextures/lambert1_Base_color.png',
 
         // onLoad callback
         function (texture) {
