b75a3cf74ca187d231351c52a726754161f92aff
Smos Bois <simondeb1505@gmail.com>
2022-06-01 20:54:08 +0200

    Updates

--- js/Sea.js
+++ js/Sea.js
@@ -1,13 +1,17 @@
+//This script is for the sea..
 function Sea(scene) {
     const textureLoader = new THREE.TextureLoader();
-    const map = textureLoader.load('resources/seaTextureTest.jpg');
+    const map = textureLoader.load('resources/textures/seaTextureTest.jpg');
 
+    //repeat the texture, so it's not a giant texture that's very pixelized
     map.wrapS = THREE.RepeatWrapping;
     map.wrapT = THREE.RepeatWrapping;
     map.repeat.set(100, 100);
 
+    //the sea itself. It's just a plane like cube
     const seaGeometry = new THREE.BoxGeometry(1000, 1, 1000);
 
+    //the material of the sea. FOr now I have the reflection set very low, I might want to edit this later
     const material = new THREE.MeshStandardMaterial();
     material.color = new THREE.Color(0x73d3f4);
     material.roughness = 0;
@@ -19,5 +23,6 @@ function Sea(scene) {
 
     scene.add(sea);
 
-    sea.position.set(0, -1, 0);
+    //I set the sea a little lower, so the boat can stay on 0 y position
+    sea.position.set(0, -0.6, 0);
 }
