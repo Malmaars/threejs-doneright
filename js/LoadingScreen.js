08405d4b48c2c3f135f91daaf4cfef7e7bcfec25
Smos Bois <simondeb1505@gmail.com>
2022-07-28 19:55:08 +0200

    Updates

---
+++ js/LoadingScreen.js
@@ -0,0 +1,170 @@
+function LoadingScreen(loadingManager, scene, texLoader, modelLoader, fontLoader, mouse, camera){
+        //Make a loadingscreen here and remove it when everything had loaded.
+
+        this.initialized = false;
+        var geometry = new THREE.BoxGeometry( 8, 1, 4 );
+
+        //console.log(geometry.parameters.width);
+        var material = new THREE.MeshBasicMaterial( { color: 0xff0000 , transparent: true, opacity: 0 } );
+        this.collider = new THREE.Mesh(geometry, material);
+        scene.add(this.collider);
+        this.collider.renderOrder = 1;
+
+
+        var platformMaterial;
+
+        texLoader.load(
+            // resource URL
+            "resources/textures/PlatformBasicTexture.png",
+    
+            // onLoad callback
+            function (texture) {
+                // in this example we create the material when the texture is loaded
+                platformMaterial = new THREE.MeshStandardMaterial({
+                    map: texture
+                });
+            },
+    
+            // onProgress callback currently not supported
+            undefined,
+    
+            // onError callback
+            function (err) {
+                console.error('An error happened.');
+            }
+        );
+    
+        this.model;
+    
+        modelLoader.load
+            ('resources/models/PlatformBasic.fbx', (function (object) {
+    
+                this.model = object;
+    
+                object.traverse(function (child) {
+                    if (child.isMesh) {
+                        platformMaterial.metalness = 0;
+                        platformMaterial.transparent = true;
+                        platformMaterial.opacity = 1;
+                        child.material = platformMaterial;
+                        
+                    }
+                })
+                scene.add(this.model);
+            }).bind(this));
+        
+            var loadingGeometry;
+            var textMesh1;
+            fontLoader.load( 'https://unpkg.com/three@0.77.0/examples/fonts/helvetiker_bold.typeface.json', function ( font ) {
+                loadingGeometry = new THREE.TextGeometry( 'LOADING...', {
+                font: font,
+                size: 120,
+                height: 5,
+                curveSegments: 12,
+                } );
+                var material = new THREE.MeshStandardMaterial();
+                material.metalness = 0;
+                textMesh1 = new THREE.Mesh( loadingGeometry, material);
+                textMesh1.scale.set(0.004,0.004,0.004);
+                textMesh1.rotation.x = Math.PI * 0.5;
+                textMesh1.rotation.y = Math.PI * 1;
+                scene.add(textMesh1);
+            } );
+
+            var startGeometry;
+            var textMesh2;
+            fontLoader.load( 'https://unpkg.com/three@0.77.0/examples/fonts/helvetiker_bold.typeface.json', function ( font ) {
+                startGeometry = new THREE.TextGeometry( 'START', {
+                font: font,
+                size: 120,
+                height: 5,
+                curveSegments: 12,
+                } );
+                var material = new THREE.MeshStandardMaterial();
+                material.metalness = 0;
+                textMesh2 = new THREE.Mesh( startGeometry, material);
+                textMesh2.scale.set(0.0000001,0.0000001,0.0000001);
+                textMesh2.rotation.x = Math.PI * 0.5;
+                textMesh2.rotation.y = Math.PI * 1;
+                scene.add(textMesh2);
+            } );
+
+    var extension;
+    modelLoader.load
+        ('resources/models/PlatformExtension.fbx', (function (object) {
+
+            extension = object;
+
+            object.traverse(function (child) {
+                if (child.isMesh) {
+                    platformMaterial.metalness = 0;
+                    platformMaterial.transparent = true;
+                    platformMaterial.opacity = 0.2;
+                    child.material = platformMaterial;
+                    
+                }
+            })
+            scene.add(extension);
+        }).bind(this));
+
+
+        this.OnStart = function(){
+
+        }
+        this.OnLoad = function(){
+            textMesh1.scale.set(0.0000001,0.0000001,0.0000001);
+            textMesh2.scale.set(0.004,0.004,0.004);
+            this.collider.name = 'Start';
+                }
+
+        this.OnProgress = function(){
+        }
+
+        this.Initialize = function(){
+            console.log('start game');
+            this.initialized = true;
+            return true;
+        }
+
+        
+        const clock = new THREE.Clock();
+        this.AnimatePlatformExtension = function(newLocation, speed){
+            var delta = clock.getDelta();
+            extension.position.lerp(newLocation, delta * speed);
+        }
+
+        var extensionPositionSet = false;
+        this.Update = function(){
+            if(this.model && textMesh1 && textMesh2){
+            this.model.position.set(camera.position.x, camera.position.y - 8, camera.position.z + 8);
+            this.model.rotation.set(0, Math.PI/2, 0);
+            this.collider.position.set(this.model.position.x, this.model.position.y, this.model.position.z);
+            textMesh1.position.set(camera.position.x + 1.65, camera.position.y - 8, camera.position.z + 7.7);
+            textMesh2.position.set(camera.position.x + 1.25, camera.position.y - 8, camera.position.z + 7.7);
+            if(extensionPositionSet == false && extension){
+                extension.position.set(this.model.position.x, this.model.position.y - 0.6, this.model.position.z);
+                extension.rotation.set(0,Math.PI/2,0);
+                extensionPositionSet = true;
+            }
+
+            if(extension){
+                console.log(extension.position);
+            }
+
+            if(extensionPositionSet != false){
+                if(mouse.linkHover == 'Start'){
+                    this.AnimatePlatformExtension(new THREE.Vector3(this.model.position.x, this.model.position.y + 1, this.model.position.z), 6);
+                }
+                else{   
+                    this.AnimatePlatformExtension(new THREE.Vector3(this.model.position.x, this.model.position.y - 0.6, this.model.position.z), 12);
+                }
+            }
+            
+            if(mouse.startGame == true){
+                this.Initialize();
+            }
+            // console.log(this.model.position);
+            // console.log(camera.position);
+            }
+        }
+}
--- js/Mouse.js
+++ js/Mouse.js
@@ -4,6 +4,9 @@ function Mouse(_camera, scene)
     var movingBoolean;
     var previousCameraPos;
     var previousMousePosition;
+
+    this.linkHover;
+    this.startGame = false;
     
     var velocity = new THREE.Vector3(0,0,0);
 
@@ -25,33 +28,55 @@ function Mouse(_camera, scene)
 
     this.PointerMoveEvent = function (event) 
     {
+        document.body.style.cursor = "grab";
         if(movingBoolean == true){
+            document.body.style.cursor = "grabbing";
             // console.log("Updating camera");
         var offset = new THREE.Vector3( event.screenX - previousMousePosition.x, 0, event.screenY - previousMousePosition.z);
 
+        offset.clampLength(-15,15);
         velocity.set(offset.x * 0.03, 0, offset.z * 0.03);
 
         // console.log(offset);
         camera.position.set(previousCameraPos.x + offset.x * 0.03, previousCameraPos.y, previousCameraPos.z + offset.z * 0.03);
         // console.log(camera.position);
         }
-                // console.log("Mouse");
+
+        
         pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
         pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
         raycaster.setFromCamera( pointer, camera )
         const intersects = raycaster.intersectObjects( scene.children, true );
                 
+        var none = true;
         for ( let i = 0; i < intersects.length; i ++ ) {
     
-            if(intersects[i].object.name == 'interactible'){
-                console.log("can interact");
+            var output = intersects[i].object.name.split('');
+            
+            if(output[0] == 'h' && output[1] == 't' && output[2] == 't' &&output[3] == 'p' && output[4] == 's' )
+            {   
+                this.linkHover = intersects[i].object.name;
+                document.body.style.cursor = "pointer";
+                //also trigger the animation somehow
+                none = false;
             }
-    
+
+            if(intersects[i].object.name == "Start"){
+                this.linkHover = intersects[i].object.name;
+                document.body.style.cursor = "pointer";
+                //also trigger the animation somehow
+                none = false;
+            }
         }
 
+        if(none == true){
+            this.linkHover = null;
+        }
+                // console.log("Mouse");
         //cast a ray to check if it's in range of any clickables
         previousMousePosition = new THREE.Vector3(event.screenX, 0, event.screenY);
+        
 
     }
 
@@ -60,6 +85,31 @@ function Mouse(_camera, scene)
         movingBoolean = true;
         previousCameraPos = camera.position;
 
+        pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+        raycaster.setFromCamera( pointer, camera )
+        const intersects = raycaster.intersectObjects( scene.children, true );
+                
+        for ( let i = 0; i < intersects.length; i ++ ) {
+    
+            var output = intersects[i].object.name.split('');
+            
+            if(output[0] == 'h' && output[1] == 't' && output[2] == 't' &&output[3] == 'p' && output[4] == 's' )
+            {   
+                movingBoolean = false;
+                //also trigger the animation somehow
+                window.open(intersects[i].object.name, intersects[i].object.name).focus();
+            }
+
+            if(intersects[i].object.name == 'Start')
+            {   
+                movingBoolean = false;
+                //also trigger the animation somehow
+                //start level
+                this.startGame = true;
+            }
+        }
     };
     this.PointerUpEvent = function()
     { 
